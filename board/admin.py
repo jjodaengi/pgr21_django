e7bb67abbf259315cdedce96f03198ebe1f27e37
Jin Suk Park <totorovirus@gmail.com>
2013-02-16 03:22:03 +0900

    관리자페이지 완성

---
+++ board/admin.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+from django.contrib import admin
+from board.models import *
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['name','description','article_count']
+    inlines = []
+
+    def article_count(self,obj):
+        return Article.objects.filter(board=obj).count()
+    article_count.short_description = u'총 글수'
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = []
+    date_hierarchy = 'reg_date'
+    list_display = ['no','board','category','subject','user','reg_date']
+    list_filter = ['board','category','is_announcement']
+
+admin.site.register(Board,BoardAdmin)
+admin.site.register(Article,ArticleAdmin)
+
+
+
--- board/models.py
+++ board/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Board(models.Model):
+
+    class Meta:
+        verbose_name = u'게시판'
+        verbose_name_plural = u'게시판'
+
+    def __unicode__(self):
+        return self.description
+
     HTML_USE = (
         ('00',u'모두막기'),
         ('01',u'부분허용'),
@@ -15,8 +23,8 @@ class Board(models.Model):
 
     article_per_page = models.IntegerField(default=20)
     page_per_view = models.IntegerField(default=10)
-    page_top = models.TextField()
-    page_bottom = models.TextField()
+    page_top = models.TextField(null=True,blank=True)
+    page_bottom = models.TextField(null=True,blank=True)
 
     use_html = models.CharField(choices=HTML_USE,max_length=2)
     use_fileupload = models.BooleanField(default=False,verbose_name=u'파일 업로드 기능 사용')
@@ -29,15 +37,22 @@ class Board(models.Model):
     file_upload_limit = models.IntegerField(default=0,verbose_name=u'개별 파일 업로드 용량')
 
     #TODO: 커스텀 필드 - 콤마로 구분하는 텍스트 필드
-    language_filter = models.TextField()
-    html_tag_filter = models.TextField()
-    ip_block_filter = models.TextField()
-    file_ext_filter = models.TextField()
+    language_filter = models.TextField(null=True,blank=True)
+    html_tag_filter = models.TextField(null=True,blank=True)
+    ip_block_filter = models.TextField(null=True,blank=True)
+    file_ext_filter = models.TextField(null=True,blank=True)
 
 
 class Article(models.Model):
+
+    def __unicode__(self):
+        return self.board.description + ' ' + unicode(self.no) + u'번 글'
+
+    class Meta:
+        verbose_name = u'글'
+        verbose_name_plural = verbose_name
     #dependencies
-    no = models.AutoField(primary_key=True)
+    no = models.IntegerField(default=0)
     board = models.ForeignKey(Board)
     memo = models.TextField(null=True)
     user = models.ForeignKey(User)
@@ -62,9 +77,16 @@ class Article(models.Model):
     vote = models.IntegerField(default=0)
     total_comment = models.IntegerField(default=0)
     division = models.IntegerField(default=0)
-
+    is_announcement = models.BooleanField(default=0,verbose_name=u'공지글')
 
 class Comment(models.Model):
+    class Meta:
+        verbose_name = u'댓글'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.board.description + u' ' + unicode(self.article.no) + u'의 댓글'
+
     board = models.ForeignKey(Board)
     article = models.ForeignKey(Article)
     ismember = models.IntegerField(default=0)
