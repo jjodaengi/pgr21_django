0774631389d443370b25acc7a897bd926988f099
Jin Suk Park <totorovirus@gmail.com>
2013-02-16 02:34:06 +0900

    장고 템플릿 적용

--- neopgr21/settings.py
+++ neopgr21/settings.py
@@ -64,7 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "G:/neopgr21/static"
+    "G:/neopgr21/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- neopgr21/views.py
+++ neopgr21/views.py
@@ -3,11 +3,11 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def index(request):
-    #TODO : 데이터베이스 셋업 및 index.html 렌더링
-    return render_to_response('index.html')
-    pass
+    #TODO : 데이터베이스 셋업 및 base.html 렌더링
+    return render_to_response('index.html',context_instance=RequestContext(request))
 
 
 
